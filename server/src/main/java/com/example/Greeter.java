aa2776295c585558aa4b6277203687b6d99578eb
Dijala Feludu <dijal.feludu@gmail.com>
2022-11-10 19:43:20 -0600

    fixed jenkins file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,9 +17,6 @@ public class Greeter {
    */
   public final String greet(final String someone) {
 
-    String password = "Admin@123456";
-    System.out.println("Hello");
-
 
     return String.format("Hello Avinash, %s!", someone);
   }
